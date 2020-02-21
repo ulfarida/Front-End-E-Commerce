07b0adf698d96dfe42987a559df1ef9445953d77
ulfarida <ulfah@alterra.id>
2020-02-21 17:49:30 +0700

    backup

---
+++ e-commerce/build/precache-manifest.d6b8b62715cb0229835db298c7b9ce87.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c804b6938a0322c5ec3521824689bca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a0ee0d66561f933012",
+    "url": "/static/css/main.b7f904ea.chunk.css"
+  },
+  {
+    "revision": "2a6c4676899a08d4e089",
+    "url": "/static/js/2.d40bce83.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.d40bce83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9a0ee0d66561f933012",
+    "url": "/static/js/main.b36766f8.chunk.js"
+  },
+  {
+    "revision": "c7bba16c195430e78514",
+    "url": "/static/js/runtime-main.baff40b1.js"
+  },
+  {
+    "revision": "e7ee8a202dd6d2b7e9ab0da812f0a99f",
+    "url": "/static/media/admin.e7ee8a20.svg"
+  },
+  {
+    "revision": "e7864b7a4cbffd6210964d300058bb70",
+    "url": "/static/media/all.e7864b7a.svg"
+  },
+  {
+    "revision": "296a6011514ed698527b32d31200500f",
+    "url": "/static/media/background-rec.296a6011.jpg"
+  },
+  {
+    "revision": "29303e5db3e5a77572776e435fda7f8a",
+    "url": "/static/media/background-rec2.29303e5d.jpg"
+  },
+  {
+    "revision": "80ebace53e3199d7d33439d3cc8041f8",
+    "url": "/static/media/background.80ebace5.jpg"
+  },
+  {
+    "revision": "2953a3e51fbc59694c6eaeb63bcf161a",
+    "url": "/static/media/background2.2953a3e5.jpg"
+  },
+  {
+    "revision": "21bb32227b42ef9d15a21d09eca6cfa0",
+    "url": "/static/media/background3.21bb3222.jpg"
+  },
+  {
+    "revision": "c2babbcba4cea2a0c648b6db8569ea3f",
+    "url": "/static/media/bath.c2babbcb.svg"
+  },
+  {
+    "revision": "8fda54680f868ff75bafeac3579c520a",
+    "url": "/static/media/best.8fda5468.svg"
+  },
+  {
+    "revision": "ddcfd30162eb003f7c05d55990dccbe1",
+    "url": "/static/media/cart.ddcfd301.svg"
+  },
+  {
+    "revision": "4d83477b74fa7eedfb31604cbcb457a9",
+    "url": "/static/media/check.4d83477b.svg"
+  },
+  {
+    "revision": "1ec51ac2d831b50fad3b92cce4a72809",
+    "url": "/static/media/clothes.1ec51ac2.svg"
+  },
+  {
+    "revision": "a36421a8af69c5c9c01f160d245944ef",
+    "url": "/static/media/delete.a36421a8.svg"
+  },
+  {
+    "revision": "4f71692e30e78eeaf5c28e5f09bc550d",
+    "url": "/static/media/diaper.4f71692e.svg"
+  },
+  {
+    "revision": "e177894a42afe93bd5ae2c01b55935da",
+    "url": "/static/media/edit.e177894a.svg"
+  },
+  {
+    "revision": "3f86dd3900a6117c46f0ac77f3ebcc27",
+    "url": "/static/media/logo.3f86dd39.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c0abc753d8af264e98fb7a0bc754c47e",
+    "url": "/static/media/logout.c0abc753.svg"
+  },
+  {
+    "revision": "3c28ef282ddc2d9ffb7ecabfb220fd4a",
+    "url": "/static/media/milk.3c28ef28.svg"
+  },
+  {
+    "revision": "c3e4b58a0428e2be7b576895d51da6b0",
+    "url": "/static/media/password.c3e4b58a.svg"
+  },
+  {
+    "revision": "09236ce1c44dd33c951ba36385ce636d",
+    "url": "/static/media/produk.09236ce1.svg"
+  },
+  {
+    "revision": "7de5e5f7d3fbd734c8d9328cc6806365",
+    "url": "/static/media/profil.7de5e5f7.svg"
+  },
+  {
+    "revision": "fc0723b6fc384bf3606441d7a8c8ef61",
+    "url": "/static/media/profile.fc0723b6.svg"
+  },
+  {
+    "revision": "1756c7bd51309f5672c6901cec4fb4e1",
+    "url": "/static/media/spoon.1756c7bd.svg"
+  },
+  {
+    "revision": "1aecf4bfee6174ed31161a5c6f7f759a",
+    "url": "/static/media/toys.1aecf4bf.svg"
+  },
+  {
+    "revision": "5374c2cc3be2e2bf11269dd73c3602f0",
+    "url": "/static/media/transaksi.5374c2cc.svg"
+  },
+  {
+    "revision": "43e76772d3cf1254f161042224ad9b96",
+    "url": "/static/media/user.43e76772.svg"
+  },
+  {
+    "revision": "4d7a598722c7faff8fd94ee29422e06a",
+    "url": "/static/media/view.4d7a5987.svg"
+  },
+  {
+    "revision": "ed002422f9fae69390138622b89f1ef1",
+    "url": "/static/media/wishlist.ed002422.svg"
+  }
+]);
